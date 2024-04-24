f99a374cf522828669c5dc09d8355226aa4e9335
Anibal Valter Muchanga <valteranibal1@gmail.com>
2024-04-24 11:11:55 +0200

    criar projecto

---
+++ src/main/java/com/APISistemaDePrestacaoDeServicos/SistemaDePrestacaoDeServicos/SistemaDePrestacaoDeServicosApplication.java
@@ -0,0 +1,13 @@
+package com.APISistemaDePrestacaoDeServicos.SistemaDePrestacaoDeServicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDePrestacaoDeServicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDePrestacaoDeServicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/APISistemaDePrestacaoDeServicos/SistemaDePrestacaoDeServicos/SistemaDePrestacaoDeServicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.APISistemaDePrestacaoDeServicos.SistemaDePrestacaoDeServicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDePrestacaoDeServicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
